93151261bc5f1eeafafc70936241bbe17ef1de5b
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-04-26 06:26:07 +0300

    Time: 562 ms (14.23%), Space: 12.8 MB (52.3%) - LeetHub

---
+++ 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        unordered_map<char, int> mp;
+        int ans=0;
+        for(int i=0; i<s.size(); i++){
+            int mx = 1;
+            for(char c='a'; c<='z'; c++){
+                if(mp[c] && abs(s[i] - c) <= k)
+                    mx = max(mx, 1+mp[c]);
+            }
+            mp[s[i]] = max(mp[s[i]], mx);
+            ans = max(ans, mp[s[i]]);
+        }
+        return ans;
+    }
+};
