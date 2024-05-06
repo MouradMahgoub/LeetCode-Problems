945a216b287e388bafd0b242cca561809d97b0d0
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-05-06 21:13:07 +0300

    Delete 0189-rotate-array directory

--- 0189-rotate-array/0189-rotate-array.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        k %= nums.size();
-        reverse(begin(nums), end(nums));
-        reverse(begin(nums), begin(nums)+k);
-        reverse(begin(nums)+k, end(nums));
-    }
-};
