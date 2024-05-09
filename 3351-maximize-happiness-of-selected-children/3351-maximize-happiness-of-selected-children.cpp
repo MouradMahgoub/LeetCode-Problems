8f55b5916bcfd334a4805e4feb96c13f9089f18e
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-05-09 08:10:07 +0300

    Time: 145 ms (94.31%), Space: 107.2 MB (32.91%) - LeetHub

--- 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -3,10 +3,8 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         sort(happiness.begin(), happiness.end(), greater<int>());
         long long ans=0;
-        for(int i=0; i<k; i++){
-            ans += happiness[i];
-            ans -= min(happiness[i], i);
-        }
+        for(int i=0; i<k; i++)
+            ans += max(0, happiness[i] - i);
         return ans;
     }
 };
