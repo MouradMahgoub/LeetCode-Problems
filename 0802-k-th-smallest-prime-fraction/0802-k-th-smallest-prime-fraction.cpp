a691739339088a1914e1ab554f8e6e4635990d10
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-05-10 23:22:16 +0300

    Time: 287 ms (60.96%), Space: 64.6 MB (52.5%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        vector<pair<double, pair<int, int>>> v((arr.size()*(arr.size()-1))/2);
-        int indx = 0;
+        priority_queue<pair<double, pair<int, int>>> pq;
         for(int i=0; i<arr.size(); i++)
-            for(int j=i+1; j<arr.size(); j++)
-                v[indx++] = {arr[i]*1.0/arr[j], {arr[i], arr[j]}};
-        sort(v.begin(), v.end());
-        return {v[k-1].second.first, v[k-1].second.second};
+            for(int j=i+1; j<arr.size(); j++){
+                pq.push({1.0*arr[i]/arr[j], {arr[i], arr[j]}});
+                if(pq.size() > k) pq.pop();
+            }
+        return {pq.top().second.first, pq.top().second.second};
     }
 };
+// vector<pair<double, pair<int, int>>> v((arr.size()*(arr.size()-1))/2);
+// int indx = 0;
+// sort(v.begin(), v.end());
+
+
 // priority_queue<pair<double, pair<int, int>>, vector<pair<double, pair<int, int>>>, greater<pair<double, pair<int, int>>>> minpq;
         // vector<int> ans(2);
         // while(k--){
