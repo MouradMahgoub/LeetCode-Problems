886d7daae9ef6e6ea9e651968434d4f16af7e40f
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-05-26 16:04:09 +0300

    Time: 261 ms (5.22%), Space: 145.5 MB (13.51%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    long long foo (int i, int k, int isEven, vector<int>& nums, vector<vector<long>>& memo) {
+        if(i == nums.size()) return isEven ? 0 : INT_MIN;
+        if(memo[i][isEven] != -1) return memo[i][isEven];
+        long long with_xor = (nums[i]^k) + foo(i+1, k, isEven^1, nums, memo); 
+        long long without_xor = nums[i] + foo(i+1, k, isEven, nums, memo); 
+        return memo[i][isEven] = max(with_xor, without_xor);
+    }
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        long long ans = 0;
-        int cnt = 0, minDiff = INT_MAX;
-        for(int num : nums){
-            int xorNum = num^k;
-            if(xorNum > num) cnt++;
-            ans += max(num, xorNum);
-            minDiff = min(minDiff, abs(num - xorNum));
-        }
-        return cnt&1 ? ans - minDiff : ans;
+        vector<vector<long>> memo(nums.size(), vector<long>(2, -1));
+        return foo(0, k, 1, nums, memo);
     }
 };
+        // long long ans = 0;
+        // int cnt = 0, minDiff = INT_MAX;
+        // for(int num : nums){
+        //     int xorNum = num^k;
+        //     if(xorNum > num) cnt++;
+        //     ans += max(num, xorNum);
+        //     minDiff = min(minDiff, abs(num - xorNum));
+        // }
+        // return cnt&1 ? ans - minDiff : ans;
