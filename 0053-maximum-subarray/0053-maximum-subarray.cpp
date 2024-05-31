718126156b64359501ce9c76a4566a8275cc8d7e
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-05-31 19:18:50 +0300

    Time: 78 ms (53.19%), Space: 70.2 MB (67.13%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int mxSum = nums[0], currSum = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(currSum < 0) currSum = 0;
+            currSum += nums[i];
+            mxSum = max(mxSum, currSum);
+        }
+        return mxSum;
+    }
+};
