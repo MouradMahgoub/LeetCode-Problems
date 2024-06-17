9a7ccd94d66ae3ece9726a03b8d8f0a5d99e23a7
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-06-17 15:11:10 +0300

    Time: 5 ms (31.5%), Space: 11.5 MB (15.19%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
+    void countingSort(vector<int> & v) {
+        unordered_map<int, int> mp;
+        for(int& i : v) mp[i]++;
+        int mn = *min_element(v.begin(), v.end());
+        int mx = *max_element(v.begin(), v.end());
+        int index = 0;
+        for(int i=mn; i<=mx; i++)
+            while(mp[i]-- > 0) v[index++] = i;
+    }
+
     int heightChecker(vector<int>& heights) {
         vector<int> v(heights);
-        sort(v.begin(), v.end());
+        countingSort(v);
+        for(int i : v) cout << i << " ";
         int ans=0;
         for(int i=0; i<v.size(); i++)
             if(v[i] != heights[i]) ans++;
