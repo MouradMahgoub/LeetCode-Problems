891a91ff773a3299b78fb5a4466e751399305a20
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-06-19 03:02:02 +0300

    Time: 61 ms (37.9%), Space: 39.6 MB (91.08%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        priority_queue<pair<int, int>> pq;
+        vector<pair<int, int>> v(profit.size());
         for(int i=0; i<profit.size(); i++)
-            pq.push({profit[i], difficulty[i]});
-        multiset<int> st(worker.begin(), worker.end());
-        int ans  = 0;
-        while(!pq.empty()){
-            auto [prof, diff] = pq.top();
-            auto it = st.lower_bound(diff);
-            while(it != st.end()){
-                ans += prof;
-                st.erase(it);
-                it = st.lower_bound(diff);
+            v[i] = {profit[i], difficulty[i]};
+        sort(v.begin(), v.end(), greater<pair<int, int>>());
+        sort(worker.begin(), worker.end(), greater<int>());
+        int ans = 0, w = 0, d = 0;
+        while(w < worker.size() && d < v.size()){
+            if(worker[w] >= v[d].second){
+                ans += v[d].first;
+                w++;                
+            }else{
+                d++;
             }
-            pq.pop();
         }
         return ans;
     }
