802b722865bffe73d33366e776d7d73500cf0546
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-06-19 20:27:17 +0300

    Time: 41 ms (46.94%), Space: 16.6 MB (39.74%) - LeetHub

--- 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int longestContinuousSubstring(string s) {
-        int l=0, ans=1;
-        for(int r=1; r<s.size(); r++){
-            if(s[r] != s[r-1]+1) l=r;
-            ans = max(ans, r-l+1);
+        int ans=1, cnt=1;
+        for(int i=1; i<s.size(); i++){
+            if(s[i] == s[i-1]+1) cnt++;
+            else cnt=1;
+            ans = max(ans, cnt);
         }
         return ans;
     }
