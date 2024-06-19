6a68e061a20fecba86bc127eda8da9e11f311ee2
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-06-20 02:45:20 +0300

    Time: 4 ms (42.31%), Space: 9.8 MB (24.52%) - LeetHub

--- 2602-maximum-enemy-forts-that-can-be-captured/2602-maximum-enemy-forts-that-can-be-captured.cpp
+++ 2602-maximum-enemy-forts-that-can-be-captured/2602-maximum-enemy-forts-that-can-be-captured.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public: 
-    int captureForts(vector<int>& forts) {
-        int ans=0;
-        for(int l=0, r=0; r<forts.size(); r++){
-            if(forts[r]){
-                if(forts[l] == -forts[r]) ans = max(ans, r-l-1);
-                l = r;
+    int foo(vector<int>& forts) {
+        int ans=0, cnt=0;
+        for(int i=0; i<forts.size(); i++){
+            if(forts[i] == 1){
+                while(++i < forts.size() && forts[i] == 0) cnt++;
+                if(i < forts.size() && forts[i] == -1) ans = max(ans, cnt);
+                cnt = 0;
+                i--;
             }
         }
+        return ans;
+    }
+    int captureForts(vector<int>& forts) {
+        int ans = 0;
+        ans = foo(forts);
+        reverse(forts.begin(), forts.end());
+        ans = max(ans, foo(forts));
         return ans;
     }
 };
