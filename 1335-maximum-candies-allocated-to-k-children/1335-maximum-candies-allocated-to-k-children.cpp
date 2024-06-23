5e41f14a4f72f939798dba2335af188b2e7a5158
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-06-24 01:41:11 +0300

    Time: 133 ms (59.02%), Space: 86.7 MB (54.78%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -7,17 +7,16 @@ public:
         return sum >= k;
     }
     int maximumCandies(vector<int>& candies, long long k) {
-        int l=0, r= 1e9;
+        int l=0, r= 1e7;
         int ans = 0 ;
-        while(l <= r){
-            int mid = l + (r-l)/2;
+        while(l < r){
+            int mid = l + (r-l+1)/2;
             if(good(candies, k, mid)){
-                l = mid + 1;
-                ans = max(ans, mid);
+                l = mid;
             }else{
                 r = mid - 1;
             }
         }
-        return ans;
+        return l;
     }
 };
