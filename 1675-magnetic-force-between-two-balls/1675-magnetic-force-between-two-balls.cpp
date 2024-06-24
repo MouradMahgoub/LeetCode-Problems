6026bac587e563d8c97e2a65789cdc76b75d81f8
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-06-24 03:11:45 +0300

    Time: 207 ms (5.3%), Space: 61.5 MB (35.34%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    // Check if we can place 'm' balls at 'position'
-    // with each ball having at least 'x' gap.
-    bool canPlaceBalls(int x, vector<int> &position, int m) {
-        // Place the first ball at the first position.
-        int prevBallPos = position[0];
-        int ballsPlaced = 1;
-
-        // Iterate on each 'position' and place a ball there if we can place it.
-        for (int i = 1; i < position.size() && ballsPlaced < m; ++i) {
-            int currPos = position[i];
-            // Check if we can place the ball at the current position.
-            if (currPos - prevBallPos >= x) {
-                ballsPlaced += 1;
-                prevBallPos = currPos;
-            }
+    bool good (vector<int>& v, int m, int ans) {
+        int curr = *v.begin();
+        m--;
+        while(m--){
+            auto it = lower_bound(v.begin(), v.end(), curr + ans);
+            if(it == v.end()) return false;
+            curr = *it;
         }
-        // If all 'm' balls are placed, return 'true'.
-        return ballsPlaced == m;
+        return true;
     }
-
-    int maxDistance(vector<int> &position, int m) {
-        int answer = 0;
-        int n = position.size();
-        sort(position.begin(), position.end());
-
-        // Initial search space.
-        int low = 1;
-        int high = ceil(position[n - 1] / (m - 1.0));
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            // If we can place all balls having a gap at least 'mid',
-            if (canPlaceBalls(mid, position, m)) {
-                // then 'mid' can be our answer,
-                answer = mid;
-                // and discard the left half search space.
-                low = mid + 1;
-            } else {
-                // Discard the right half search space.
-                high = mid - 1;
-            }
+    int maxDistance(vector<int>& v, int m) {
+        sort(v.begin(), v.end());
+        int l=1, r=1e9;
+        while(l < r) {
+            int mid = l + (r-l+1)/2;
+            if(good(v, m, mid)) l = mid;
+            else r = mid - 1;
         }
-        return answer;
+        return l;
     }
 };
