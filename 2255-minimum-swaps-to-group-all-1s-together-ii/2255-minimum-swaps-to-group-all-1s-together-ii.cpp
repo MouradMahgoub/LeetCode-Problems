43a2c74c89653ce4a424da80db4c4e7a4bb06b9f
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-03 02:59:58 +0300

    Time: 80 ms (16.25%), Space: 83.1 MB (58.03%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int k = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        int cnt = accumulate(nums.begin(), nums.begin() + k, 0);
-        int mx = cnt;
-        for (int i = k; i < n + k; ++i) {
-            cnt += nums[i % n] - nums[(i - k + n) % n];
-            mx = max(mx, cnt);
+        int tot1s = 0, curr1s=0;
+        for(int i : nums) if(i) tot1s++;
+        for(int i=0; i<tot1s; i++) if(nums[i]) curr1s++;
+        int ans=1e9;
+        for(int l=0, r=tot1s-1; l<nums.size(); ){
+            ans = min(ans, tot1s - curr1s);
+            curr1s -= nums[l++];
+            curr1s += nums[(++r)%(nums.size())];
         }
-        return k - mx;
+        return ans;
     }
 };
