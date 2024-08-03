9e361d0cb2026f78fee802a13ec2b37fd47abaad
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-03 23:46:58 +0300

    Time: 5 ms (52.36%), Space: 11.9 MB (24.95%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        int n = path.size();
+        string simpath = "/";
+        for(int i=1; i<n; i++){
+            if(path[i] == '/' && path[i-1] == '/') continue;
+            simpath += path[i];
+        }
+        n = simpath.size();
+        for(int i=1; i<n; i++){
+            string temp = "";
+            while(i < n && simpath[i] != '/'){
+                temp += simpath[i++];
+            }            
+            if(temp == ".") continue;
+            if(temp == "..") {
+                if(!st.empty()) st.pop();
+                continue;
+            }
+            st.push(temp);
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans = ("/" + st.top() + ans);
+            st.pop();
+        } 
+        if(ans.size() == 0) ans = "/";
+        return ans;
+    }
+};
