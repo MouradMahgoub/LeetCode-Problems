9aee498891c87e0f391d571d95d7ada16b6d6cc7
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-04 00:17:33 +0300

    Time: 6 ms (46.57%), Space: 11.9 MB (24.95%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -2,31 +2,50 @@ class Solution {
 public:
     string simplifyPath(string path) {
         stack<string> st;
-        int n = path.size();
-        string simpath = "/";
-        for(int i=1; i<n; i++){
-            if(path[i] == '/' && path[i-1] == '/') continue;
-            simpath += path[i];
+        string curr = "";
+        path += '/';
+        for(char c : path){
+            if(c == '/'){
+                if(curr == "..") {
+                    if(!st.empty()) 
+                        st.pop();
+                }else if(curr != "." && curr != ""){
+                    st.push(curr);
+                }
+                curr = "";
+            }else curr += c;
         }
-        n = simpath.size();
-        for(int i=1; i<n; i++){
-            string temp = "";
-            while(i < n && simpath[i] != '/'){
-                temp += simpath[i++];
-            }            
-            if(temp == ".") continue;
-            if(temp == "..") {
-                if(!st.empty()) st.pop();
-                continue;
-            }
-            st.push(temp);
-        }
+        
         string ans = "";
         while(!st.empty()){
-            ans = ("/" + st.top() + ans);
+            ans =  '/' + st.top() + ans;
             st.pop();
-        } 
-        if(ans.size() == 0) ans = "/";
+        }
+        if(ans == "") ans = "/";
         return ans;
     }
 };
+        // string simpath = "/";
+        // for(int i=1; i<n; i++){
+        //     if(path[i] == '/' && path[i-1] == '/') continue;
+        //     simpath += path[i];
+        // }
+        // n = simpath.size();
+        // for(int i=1; i<n; i++){
+        //     string temp = "";
+        //     while(i < n && simpath[i] != '/'){
+        //         temp += simpath[i++];
+        //     }            
+        //     if(temp == ".") continue;
+        //     if(temp == "..") {
+        //         if(!st.empty()) st.pop();
+        //         continue;
+        //     }
+        //     st.push(temp);
+        // }
+        // string ans = "";
+        // while(!st.empty()){
+        //     ans = ("/" + st.top() + ans);
+        //     st.pop();
+        // } 
+        // if(ans.size() == 0) ans = "/";
