327e48492a7e6d46351f689b277c98c3396457d6
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-04 23:19:35 +0300

    Time: 122 ms (82.52%), Space: 80.8 MB (86.87%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.cpp
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -5,14 +5,20 @@ public:
         vector<pair<int, double>> v(n);
         for(int i=0; i<n; i++) v[i] = {position[i], (target-position[i])/(1.0*speed[i])};
         sort(v.begin(), v.end());
-        int ans=1;
-        for(int i=n-1; i>=1; i--){
-            if(v[i].second >= v[i-1].second){
-                v[i-1].second = v[i].second;
-                continue;
-            }
-            ans++;
+        stack<double> st;
+        for(int i=n-1; i>=0; i--){
+            if(!st.empty() && v[i].second <= st.top()) continue;
+            st.push(v[i].second);
         }
-        return ans;
+        return st.size();
     }
 };
+        // int ans=1;
+        // for(int i=n-1; i>=1; i--){
+        //     if(v[i].second >= v[i-1].second){
+        //         v[i-1].second = v[i].second;
+        //         continue;
+        //     }
+        //     ans++;
+        // }
+        // return ans;
