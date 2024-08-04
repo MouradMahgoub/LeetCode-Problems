e9cd3a691362d9e06406f9747d06b5c505106b18
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-04 15:26:31 +0300

    Time: 66 ms (89.21%), Space: 11.5 MB (81.35%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        priority_queue<int> subArr;
-        for(int i=0; i<n; i++){
-            int sub = 0;
-            for(int j=i; j<n; j++){
-                sub += nums[j];
-                subArr.push(-sub);
-            }
+        priority_queue<pair<int, int>> pq;
+        for(int i=0; i<n; i++) pq.push({-nums[i], i});
+        int ans=0, mod = 1e9 + 7;;
+        for(int i=1; i<=right; i++){
+            auto [val, indx] = pq.top(); pq.pop();
+            if(i >= left) ans = (ans + -val)%mod;
+            if(indx < n-1) pq.push({val-nums[indx+1], indx+1});
         }
-        int temp = left-1;
-        while(temp--) subArr.pop();
-        temp = right-left+1;
-        int ans = 0;
-        while(temp--){
-            ans = (ans + -subArr.top())%(1000000007);
-            subArr.pop();
-        } 
         return ans;
     }
 };
+        // for(int i=0; i<n; i++){
+        //     int sub = 0;
+        //     for(int j=i; j<n; j++){
+        //         sub += nums[j];
+        //         subArr.push(-sub);
+        //     }
+        // }
+        // int temp = left-1;
+        // while(temp--) subArr.pop();
+        // temp = right-left+1;
+        // int ans = 0;
+        // while(temp--){
+        //     ans = (ans + -subArr.top())%(1000000007);
+        //     subArr.pop();
+        // } 
