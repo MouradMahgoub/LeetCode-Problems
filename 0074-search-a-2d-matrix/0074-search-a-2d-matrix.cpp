4d560ee0e6c53538d5e48852d497e67551781cd3
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-07 19:25:41 +0300

    Time: 3 ms (66.34%), Space: 12.1 MB (34.71%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int l=0, r=int(matrix.size());
-        while(l < r){
-            int mid = l + (r-l)/2;
-            if(matrix[mid][0] <= target){
-                l = mid + 1;
-            }else{
-                r = mid;
-            }
-        }
-        if(l == 0) return false;
-        int row = l - 1;
-        l=0, r=int(matrix[0].size())-1;
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int n=arr.size(), m=arr[0].size();
+        int l=0, r=n-1;
+        int row = -1;
         while(l <= r){
             int mid = l + (r-l)/2;
-            if(matrix[row][mid] == target){
-                return true;
-            }else if(matrix[row][mid] > target){
-                r = mid - 1;
-            }else{
-                l = mid + 1;
+            if(arr[mid][0] <= target) {
+                row = mid;
+                l = mid+1;
             }
+            else r = mid-1;
+        }
+        if(row == -1) return false;
+        int col = -1;
+        l=0, r=m-1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(arr[row][mid] == target) return true;
+            else if(arr[row][mid] < target) l = mid+1;
+            else r = mid-1;
         }
         return false;
     }
