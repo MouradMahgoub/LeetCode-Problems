32ef9a95ba1ec3e66b5b8fcf34ac7e56b0e216c5
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-07 19:54:29 +0300

    Time: 7 ms (17.1%), Space: 12.9 MB (22.38%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,11 +4,8 @@ public:
         int l=0, r=nums.size()-1;
         while(l < r){
             int mid = l + (r-l)/2;
-            if(nums[mid] > nums[r]){
-                l = mid + 1;
-            }else{
-                r = mid;
-            }
+            if(nums[mid] <= nums[r]) r = mid;
+            else l = mid+1;
         }
         return nums[l];
     }
