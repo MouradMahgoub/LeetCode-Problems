ae9183ba815bbd89a89003582c897c6c1061f5e4
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-10 02:52:15 +0300

    Time: 2 ms (65.04%), Space: 11.1 MB (53.25%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -1,38 +1,56 @@
 class Solution {
 public:
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int ans = 0;
-        int m = grid.size();
-        int n = grid[0].size();
-        for (int row = 0; row + 2 < m; row++) {
-            for (int col = 0; col + 2 < n; col++) {
-                if (isMagicSquare(grid, row, col)) {
-                    ans++;
+        int n=grid.size(), m=grid[0].size();
+        int ans=0;
+        for(int i=0; i<n-2; i++){
+            for(int j=0; j<m-2; j++){
+                int sum=0; 
+                for(int ii=i; ii<i+3; ii++) sum += grid[ii][j];
+                int temp=0, flag=0;
+                //sum rows
+                for(int ii=i; ii<i+3; ii++){
+                    temp=0;
+                    for(int jj=j; jj<j+3; jj++){
+                        temp += grid[ii][jj];
+                    }
+                    if(temp != sum){
+                        flag=1; break;
+                    } 
                 }
+                //sum cols
+                for(int jj=j; jj<j+3; jj++){
+                    temp=0;
+                    for(int ii=i; ii<i+3; ii++){
+                        temp += grid[ii][jj];
+                    }
+                    if(temp != sum){
+                        flag=1; break;
+                    } 
+                }
+                // sum diagonals
+                temp=0;
+                for(int ii=i, jj=j; ii<i+3 && jj<j+3; ii++, jj++)
+                    temp += grid[ii][jj];  
+                if(temp != sum) flag=1;
+                
+                temp=0;
+                for(int ii=i, jj=j+2; ii<i+3 && jj>=j; ii++, jj--)
+                    temp += grid[ii][jj];  
+                if(temp != sum) flag=1; 
+
+                if(flag) continue;
+
+                map<int, int> mp;
+                for(int ii=i; ii<i+3; ii++)
+                    for(int jj=j; jj<j+3; jj++)
+                        mp[grid[ii][jj]]++;
+                int cnt=0;
+                for(int ii=1; ii<=9; ii++) 
+                    if(mp[ii]) cnt++;
+                if(cnt == 9) ans++;
             }
         }
         return ans;
-    }
-
-private:
-    bool isMagicSquare(vector<vector<int>>& grid, int row, int col) {
-        // The sequences are each repeated twice to account for
-        // the different possible starting points of the sequence
-        // in the magic square
-        string sequence = "2943816729438167";
-        string sequenceReversed = "7618349276183492";
-
-        string border = "";
-        // Flattened indices for bordering elements of 3x3 grid
-        vector<int> borderIndices = {0, 1, 2, 5, 8, 7, 6, 3};
-        for (int i : borderIndices) {
-            int num = grid[row + i / 3][col + (i % 3)];
-            border += to_string(num);
-        }
-
-        // Make sure the sequence starts at one of the corners
-        return (grid[row][col] % 2 == 0 && grid[row + 1][col + 1] == 5 &&
-                (sequence.find(border) != string::npos ||
-                 sequenceReversed.find(border) != string::npos));
     }
 };
