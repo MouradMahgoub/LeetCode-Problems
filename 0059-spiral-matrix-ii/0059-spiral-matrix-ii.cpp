bdb14e97c45f65fb5327e6e0624d9f2bad1c5801
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-13 10:06:59 +0300

    Time: 0 ms (100%), Space: 8.1 MB (19.02%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> ans(n, vector<int>(n));
-        int curr=1;
         int l=0, r=n-1;
-        int t=0, b=n-1;
-        while(1){
-            for(int i=l; i<=r; i++) ans[t][i] = curr++;
+        int t=0, d=n-1;
+        vector<vector<int>> arr(n, vector<int>(n));
+        int cnt = 1;
+        while(l <= r && t <= d){
+            //top row
+            for(int i=l; i<=r; i++) arr[t][i] = cnt++;
             t++;
-            if(l > r || t > b) break;
-            for(int i=t; i<=b; i++) ans[i][r] = curr++;
+            //right col
+            for(int i=t; i<=d; i++) arr[i][r] = cnt++;
             r--;
-            if(l > r || t > b) break;
-            for(int i=r; i>=l; i--) ans[b][i] = curr++;
-            b--;
-            if(l > r || t > b) break;
-            for(int i=b; i>=t; i--) ans[i][l] = curr++;
+            //down row
+            for(int i=r; i>=l;i--) arr[d][i] = cnt++;
+            d--;
+            //left col
+            for(int i=d; i>=t; i--) arr[i][l] = cnt++;
             l++;
-            if(l > r || t > b) break;
         }
-        return ans;
+        return arr;
     }
 };
