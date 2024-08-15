cad356ea2513c06308a85f2368bd7e2fa010be1d
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-15 19:07:06 +0300

    Time: 88 ms (8.48%), Space: 70 MB (7.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int target, n;
     vector<int> candidates;
-    set<vector<int>> ans;
+    vector<vector<int>> ans;
 
     void foo(int i, int sum, vector<int> temp) {
-        if(sum == target) ans.insert(temp);
-        if(sum >= target) return;
-        for(int j=i; j<n; j++){
-            temp.push_back(candidates[j]);
-            foo(j, sum+candidates[j], temp);
+        if(sum == target) ans.push_back(temp);
+        if(sum >= target || i >= n) return;
+
+            foo(i+1, sum, temp);
+
+            temp.push_back(candidates[i]);
+            foo(i, sum+candidates[i], temp);
             temp.pop_back();
-        }
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         this->target = target;
         this->candidates = candidates;
         this->n = candidates.size();
-        for(int i=0; i<n; i++) foo(i, 0, {});
-        vector<vector<int>> ans2(ans.begin(), ans.end());
-        return ans2;
+        foo(0, 0, {});
+        return ans;
     }
 };
