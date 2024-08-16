15d0fffd0ced392fb381df9e54b5d9eeddea854a
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-16 09:15:15 +0300

    Time: 228 ms (66.51%), Space: 107.6 MB (76.89%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrs) {
-        vector<pair<int, int>> mins(arrs.size()), maxs(arrs.size());
-        for(int i=0; i<arrs.size(); i++){
-            mins[i] = {arrs[i][0], i};
-            maxs[i] = {arrs[i][arrs[i].size()-1], i};
+        int mini = arrs[0][0];
+        int maxi = arrs[0].back();
+        int ans=0;
+        for(int i=1; i<arrs.size(); i++){
+            ans = max(ans, abs(arrs[i].back() - mini));
+            ans = max(ans, abs(maxi - arrs[i][0]));
+            mini = min(mini, arrs[i][0]);
+            maxi = max(maxi, arrs[i].back());
         }
-        sort(mins.begin(), mins.end());
-        sort(maxs.rbegin(), maxs.rend());
-        int ans=0, i=0;
-        while(maxs[0].second == mins[i].second) i++;      
-        ans = abs(maxs[0].first - mins[i].first);
-        i=0;
-        while(mins[0].second == maxs[i].second) i++;      
-        ans = max(ans, abs(mins[0].first - maxs[i].first));
         return ans;
     }
 };
+        // vector<pair<int, int>> mins(arrs.size()), maxs(arrs.size());
+        // for(int i=0; i<arrs.size(); i++){
+        //     mins[i] = {arrs[i][0], i};
+        //     maxs[i] = {arrs[i][arrs[i].size()-1], i};
+        // }
+        // sort(mins.begin(), mins.end());
+        // sort(maxs.rbegin(), maxs.rend());
+        // int ans=0, i=0;
+        // while(maxs[0].second == mins[i].second) i++;      
+        // ans = abs(maxs[0].first - mins[i].first);
+        // i=0;
+        // while(mins[0].second == maxs[i].second) i++;      
+        // ans = max(ans, abs(mins[0].first - maxs[i].first));
+        // return ans;
