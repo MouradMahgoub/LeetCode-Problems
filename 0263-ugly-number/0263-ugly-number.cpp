66b772269b7631b27ae542d30585542685f97b17
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-18 22:05:06 +0300

    Time: 0 ms (100%), Space: 7.5 MB (9.61%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n <= 0) return false;
-        for(int i=2; i<=5; i++)
+        for(int i=2; i<=5 && n; i++)
             while(n%i == 0) n /= i;
         return n == 1;
     }
