94a2e873982442040db80ff809cfb259cebeee4e
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-18 23:32:47 +0300

    Time: 2 ms (63.82%), Space: 9.1 MB (56.35%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> temp;
-    void dfs (int indx, int len, vector<int>& nums) {
+
+    void dfs (int i, vector<int>& nums) {
         // if(len == k) 
-        if(len > nums.size()) return;
-        ans.push_back(temp);
-        for(int i=indx; i<nums.size(); i++){
+        if(i == nums.size()) {
+            ans.push_back(temp);
+            return;
+        } 
+
+        // for(int i=indx; i<nums.size(); i++){
             temp.push_back(nums[i]);
-            dfs(i+1, len+1, nums);
+            dfs(i+1, nums);
             temp.pop_back();
+
             while(i < nums.size()-1 && nums[i] == nums[i+1]) i++;
-        }
+            dfs(i+1, nums);
+        // }
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         // for(int k=0; k<=nums.size(); k++)
-            dfs(0, 0, nums);
+        dfs(0, nums);
         return ans;
     }
 };
