9ceba7b52151207d2482895a1d00eb81dabc009f
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-20 07:14:01 +0300

    Time: 3 ms (38.64%), Space: 8.5 MB (35.37%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> ans;
+    string map[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    string temp;
+
+    void dfs (int i, string digits) {
+        if(i == digits.size()){
+            if(!temp.empty()) ans.push_back(temp);
+            return;
+        } 
+        for(char c : map[digits[i] - '0']){
+            temp.push_back(c);
+            dfs(i+1, digits);
+            temp.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        dfs(0, digits);
+        return ans;
+    }
+};
