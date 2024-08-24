517aa4b7a80e7234df9b12a3fa54c9a1f94495e3
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-24 04:32:03 +0300

    Time: 3 ms (39.45%), Space: 7.6 MB (15.02%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -3,6 +3,6 @@ public:
     int findComplement(int num) {
         for(int i=0; i<=33; i++)
             if((pow(2, i)) > num) return (pow(2, i)) - num - 1;
-        return 0;
+        return -1;
     }
 };
