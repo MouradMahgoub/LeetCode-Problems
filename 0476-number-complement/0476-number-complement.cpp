e4e9d509cdf2d1090dfb8b4b7cbd4572cde7c008
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-08-24 04:31:27 +0300

    Time: 0 ms (100%), Space: 7.6 MB (15.02%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        for(int i=0; i<=33; i++)
+            if((pow(2, i)) > num) return (pow(2, i)) - num - 1;
+        return 0;
+    }
+};
