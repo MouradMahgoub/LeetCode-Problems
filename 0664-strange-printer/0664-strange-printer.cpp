b78ff1f934e4331a4b7d637a934d1954b0ff56e2
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-09-02 04:14:55 +0300

    Time: 137 ms (6.81%), Space: 73.5 MB (5.94%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,60 +1,26 @@
 class Solution {
 public:
-    int strangePrinter(string s) {
-        // Preprocess the string to remove consecutive duplicate characters
-        s = removeDuplicates(s);
-        int n = s.length();
+    int memo[105][105];
 
-        // dp[i][j] represents the minimum number of turns to print s[i] to s[j]
-        vector<vector<int>> minTurns(n, vector<int>(n, 0));
-
-        // Initialize base case
-        for (int i = 0; i < n; i++) {
-            // It takes 1 turn to print a single character
-            minTurns[i][i] = 1;
+    int foo (int start, int end, string s) {
+         if (start > end) return 0;
+        if(memo[start][end] != -1) return memo[start][end];
+        int ans = 1 + foo(start+1, end, s);
+        for(int k=start+1; k<=end; k++){
+            if(s[start] == s[k])
+                ans = min(ans, foo(start+1, k-1, s) + foo(k, end, s));
         }
-
-        // Fill the dp table
-        for (int length = 2; length <= n; length++) {
-            for (int start = 0; start + length - 1 < n; start++) {
-                int end = start + length - 1;
-
-                // Initialize with worst case: print each character separately
-                minTurns[start][end] = length;
-
-                // Try all possible splits and find the minimum
-                for (int split = 0; split < length - 1; split++) {
-                    int totalTurns = minTurns[start][start + split] +
-                                     minTurns[start + split + 1][end];
-
-                    // If the characters at the split and end match, we can save
-                    // one turn
-                    if (s[start + split] == s[end]) {
-                        totalTurns--;
-                    }
-
-                    minTurns[start][end] =
-                        min(minTurns[start][end], totalTurns);
-                }
-            }
-        }
-
-        // Return the minimum turns needed to print the entire string
-        return minTurns[0][n - 1];
+        return memo[start][end] = ans; 
     }
 
-private:
-    string removeDuplicates(string& s) {
-        string uniqueChars;
-        int i = 0;
-        while (i < s.length()) {
-            char currentChar = s[i];
-            uniqueChars += currentChar;
-            // Skip all consecutive occurrences of the current character
-            while (i < s.length() && s[i] == currentChar) {
-                i++;
-            }
+    int strangePrinter(string s) {
+        string temp = "";
+        for(int i=0; i<s.size(); i++) {
+            temp += s[i];
+            while(i < s.size()-1 && s[i] == s[i+1]) i++;
         }
-        return uniqueChars;
+        s = temp;
+        memset(memo, -1, sizeof(memo));
+        return foo(0, s.size()-1, s);
     }
 };
