88bda27fff7cc42eb2da33dda77fdb75d88a015b
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-01 00:42:15 +0300

    Time: 0 ms (100%), Space: 41.6 MB (61.66%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,29 +9,38 @@
  * }
  */
 class Solution {
-    public ListNode reverse (ListNode head) {
-        ListNode prev = null, curr = head;
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
+    // public ListNode reverse (ListNode head) {
+    //     ListNode prev = null, curr = head;
+    //     while(curr != null){
+    //         ListNode next = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     return prev;
+    // }
 
-    public ListNode removeNthNode (ListNode head, int n) {
-        if(n == 1) return head.next;
-        ListNode curr = head;
-        for(int i=0; i<n-2; i++) curr = curr.next;
-        curr.next = curr.next.next;
-        return head;
-    }
+    // public ListNode removeNthNode (ListNode head, int n) {
+    //     if(n == 1) return head.next;
+    //     ListNode curr = head;
+    //     for(int i=0; i<n-2; i++) curr = curr.next;
+    //     curr.next = curr.next.next;
+    //     return head;
+    // }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        head = reverse(head);
-        head = removeNthNode(head, n);
-        head = reverse(head);
-        return head;
+        ListNode dummy = new ListNode(0, head);
+        ListNode l = dummy, r = dummy;
+        for(int i=0; i<n; i++) r = r.next;
+        while(r.next != null){
+            l = l.next;
+            r = r.next;
+        } 
+        l.next = l.next.next;
+        return dummy.next;
+        // head = reverse(head);
+        // head = removeNthNode(head, n);
+        // head = reverse(head);
+        // return head;
     }
 }
