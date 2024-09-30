08057a10d22c3a453ec17634379d05d080f118bb
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-09-30 16:18:40 +0300

    Time: 28 ms (52.33%), Space: 24.2 MB (10.62%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -19,32 +19,41 @@ public:
         return slow;
     }
 
-    ListNode* reverse (ListNode* head) {
-        ListNode * prev = NULL, *curr = head;
-        while(curr){
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+    ListNode* reverse (ListNode* prev, ListNode* curr) {
+        if(!curr) return prev;
+        ListNode* newHead = reverse (curr, curr->next);
+        curr->next = prev;
+        return newHead;
+
+        // ListNode * prev = NULL, *curr = head;
+        // while(curr){
+        //     ListNode* next = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+        // return prev;
     }
 
-    void merge (ListNode* l1, ListNode* l2) {
-        while(l1 && l2){
-            ListNode* l1Next = l1->next, *l2Next = l2->next;
-            l1->next = l2;
-            l2->next = l1Next;
-            l1 = l1Next;
-            l2 = l2Next;
-        }
+    ListNode* merge (ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        l1->next = merge(l2, l1->next);
+        return l1;
+        // while(l1 && l2){
+        //     ListNode* l1Next = l1->next, *l2Next = l2->next;
+        //     l1->next = l2;
+        //     l2->next = l1Next;
+        //     l1 = l1Next;
+        //     l2 = l2Next;
+        // }
     }
 
     void reorderList(ListNode* head) {
         ListNode* middleNode = getMiddle(head);
         ListNode* l1 = head, *l2 = middleNode->next;
         middleNode->next = NULL;
-        l2 = reverse(l2);
+        l2 = reverse(NULL, l2);
         merge(l1, l2);
         return;
     }
