39c192a8d9e770f89a7c04bd095d60b10a044865
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-13 21:58:34 +0300

    Time: 4 ms (39.03%), Space: 42 MB (54.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,21 +1,17 @@
 class Solution {
     List<String> ans = new LinkedList<String>();
     String[] mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    
     private void foo (int i, String temp, String digits) {
-        if(temp.length() > 0 && temp.length() == digits.length()){
-            ans.add(temp);
+        if(i == digits.length()){
+            if(temp.length() > 0) ans.add(temp);
             return;
         }
-        for(int j=i+1; j<digits.length(); j++){
-                for(int k=0; k<mp[digits.charAt(j) - '0'].length(); k++){
-                    foo(j, temp + mp[digits.charAt(j) - '0'].charAt(k), digits);
-                }
-        }
+        for(int k=0; k<mp[digits.charAt(i) - '0'].length(); k++)
+            foo(i+1, temp + mp[digits.charAt(i) - '0'].charAt(k), digits);
     }
 
     public List<String> letterCombinations(String digits) {
-        foo(-1, "", digits);
+        foo(0, "", digits);
         return ans;
     }
 }
