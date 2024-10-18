c684af7665b588dc25b0b8c24db61d1830f8b4ea
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-18 14:50:34 +0300

    Time: 2 ms (83.53%), Space: 44 MB (95.2%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -18,18 +18,16 @@ class Solution {
         return merge(leftPart, rightPart);
     }
     
-    private ListNode merge (ListNode n1, ListNode n2) {
-        if(n1 == null) return n2; 
-        if(n2 == null) return n1;
-        ListNode dummy = new ListNode();
-        if(n1.val < n2.val) {
-            dummy = n1;
-            dummy.next = merge(n1.next, n2); 
+    private ListNode merge (ListNode l1, ListNode l2) {
+        if(l1 == null) return l2; 
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = merge(l1.next, l2);
+            return l1;
         }else{
-            dummy = n2;
-            dummy.next = merge(n1, n2.next);
+            l2.next = merge(l1, l2.next);
+            return l2;
         }
-        return dummy;
     }
         // ListNode dummy = new ListNode();
         // ListNode p1 = n1, p2 = n2, curr = dummy;
