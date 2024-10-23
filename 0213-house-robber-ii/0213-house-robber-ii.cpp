ec71bc513749cf50c7e7e5bfd7c591c8557f252a
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-23 21:58:07 +0300

    Time: 0 ms (100%), Space: 9.8 MB (12.21%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // if(nums.size() == 1) return nums[0];
+        return max(rob(0, nums.size()-2, nums), rob(1, nums.size()-1, nums));
+    }
+    int rob (int start, int end, vector<int>& nums) {
+        if(start > end) return nums[0];
+        if(start == end) return nums[start];
+        vector<int> dp(nums.size(), 0);
+        dp[start] = nums[start];
+        dp[start+1] = max(nums[start], nums[start+1]);
+        for(int i=start+2; i<=end; i++)
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        return dp[end];
+    }
+};
