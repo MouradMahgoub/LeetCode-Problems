7a164a5ac9c69c0ad8375fb6e56e818e7459fec4
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-23 22:20:41 +0300

    Time: 1 ms (78.42%), Space: 16.7 MB (52.93%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    vector<int> memo;
     int minCostClimbingStairs(vector<int>& cost) {
-        memo = vector<int>(cost.size()+1, -1);
-        return min(foo(0, cost), foo(1, cost));
-    }
-    int foo (int i, vector<int>& cost) {
-        if(i > cost.size()-1) return 0;
-        if(memo[i] != -1) return memo[i];
-        memo[i] = cost[i] + min(foo(i+1, cost), foo(i+2, cost));
-        return memo[i];
+        if(cost.size() == 1) return cost[0];
+        vector<int> dp(cost.size()+1, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2; i<cost.size(); i++)
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        return min(dp[cost.size()-1], dp[cost.size()-2]);
     }
 };
+    // vector<int> memo;
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     memo = vector<int>(cost.size()+1, -1);
+    //     return min(foo(0, cost), foo(1, cost));
+    // }
+    // int foo (int i, vector<int>& cost) {
+    //     if(i > cost.size()-1) return 0;
+    //     if(memo[i] != -1) return memo[i];
+    //     memo[i] = cost[i] + min(foo(i+1, cost), foo(i+2, cost));
+    //     return memo[i];
+    // }
