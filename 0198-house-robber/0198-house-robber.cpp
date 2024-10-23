f45203f0dad79c75fee2f6b954d871d36271528e
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-23 21:35:36 +0300

    Time: 0 ms (100%), Space: 9.4 MB (62.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    vector<int> memo;
     int rob(vector<int>& nums) {
-        memo = vector<int>(nums.size(), -1);
-        return rob(0, nums);
-    }
-    int rob (int i, vector<int>& nums) {
-        if(i > nums.size()-1) return 0;
-        if(memo[i] != -1) return memo[i];
-        memo[i] = max(nums[i] + rob(i+2, nums), rob(i+1, nums));
-        return memo[i]; 
+        int prev1 = 0, prev2=0;
+        for(int num : nums){
+            int temp = prev1;
+            prev1 = max(prev2 + num, prev1);
+            prev2 = temp;
+        }
+        return prev1;
     }
 };
+        // vector<int> dp(nums.size()+1, 0);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+        // for(int i=2; i<nums.size(); i++)
+        //     dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        // return dp[nums.size()-1];
+    // vector<int> memo;
+    // int rob(vector<int>& nums) {
+        // memo = vector<int>(nums.size(), -1);
+        // return rob(0, nums);
+    // }
+    // int rob (int i, vector<int>& nums) {
+    //     if(i > nums.size()-1) return 0;
+    //     if(memo[i] != -1) return memo[i];
+    //     memo[i] = max(nums[i] + rob(i+2, nums), rob(i+1, nums));
+    //     return memo[i]; 
+    // }
