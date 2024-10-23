e56c4bf148722e8a37c761085cc4645eb747edb4
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-23 22:22:29 +0300

    Time: 0 ms (100%), Space: 16.8 MB (52.93%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -3,11 +3,11 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         if(cost.size() == 1) return cost[0];
         vector<int> dp(cost.size()+1, 0);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i=2; i<cost.size(); i++)
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
-        return min(dp[cost.size()-1], dp[cost.size()-2]);
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i=2; i<=cost.size(); i++)
+            dp[i] = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2]);
+        return dp[cost.size()];
     }
 };
     // vector<int> memo;
