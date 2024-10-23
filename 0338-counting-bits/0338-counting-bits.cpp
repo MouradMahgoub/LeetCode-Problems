918d06aac77f71ef5f3b4dfe07b7998ee3581b15
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-23 19:35:55 +0300

    Time: 0 ms (100%), Space: 9.2 MB (77.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int countOnes (int x) {
-        int cnt = 0;
-        while(x != 0){
-            cnt += (x&1 == 1);
-            x >>= 1;
-        }
-        return cnt;
-    }
     vector<int> countBits(int n) {
-        vector<int> ans(n+1);
-        for(int i=0; i<=n; i++)
-            ans[i] = countOnes(i);
-        return ans;
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        int offset = 1;
+        for(int i=1; i<=n; i++){
+            if(offset*2 == i)
+                offset = i;
+            dp[i] = 1 + dp[i-offset];
+        }
+        return dp;
     }
 };
+    // int countOnes (int x) {
+    //     int cnt = 0;
+    //     while(x != 0){
+    //         cnt += (x&1 == 1);
+    //         x >>= 1;
+    //     }
+    //     return cnt;
+    // }
+        // vector<int> ans(n+1);
+        // for(int i=0; i<=n; i++)
+        //     ans[i] = countOnes(i);
+        // return ans;
