ba4c469d82b4b08352f7a524c37c3dd1e98b0d87
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-23 21:58:33 +0300

    Time: 0 ms (100%), Space: 9.8 MB (35.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // if(nums.size() == 1) return nums[0];
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[1], nums[0]);
         return max(rob(0, nums.size()-2, nums), rob(1, nums.size()-1, nums));
     }
     int rob (int start, int end, vector<int>& nums) {
-        if(start > end) return nums[0];
-        if(start == end) return nums[start];
+        // if(start > end) return nums[0];
+        // if(start == end) return nums[start];
         vector<int> dp(nums.size(), 0);
         dp[start] = nums[start];
         dp[start+1] = max(nums[start], nums[start+1]);
