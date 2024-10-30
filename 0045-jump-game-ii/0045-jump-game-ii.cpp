05e1d6c972842b9dc56df2f3c9448ee379b858e0
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-10-30 14:58:40 +0300

    Time: 0 ms (100%), Space: 19.4 MB (44.34%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n, 1e9);
-        dp[0] = 0;
-        for(int i=0; i<n; i++)
-            for(int j=i+1; j<=i+nums[i] && j<n; j++)
-                dp[j] = min(dp[j], dp[i]+1);
-        return dp[n-1];
+        int l=0, r=0, ans=0;
+        while(r < n-1){
+            int widthOfWindow = 0;
+            for(int i=l; i<=r; i++)
+                widthOfWindow = max(widthOfWindow, i+nums[i]-r);
+            ans++;
+            l = r+1;
+            r += widthOfWindow;
+        }
+        return ans;
     }
 };
+        // vector<int> dp(n, 1e9);
+        // dp[0] = 0;
+        // for(int i=0; i<n; i++)
+        //     for(int j=i+1; j<=i+nums[i] && j<n; j++)
+        //         dp[j] = min(dp[j], dp[i]+1);
+        // return dp[n-1];
