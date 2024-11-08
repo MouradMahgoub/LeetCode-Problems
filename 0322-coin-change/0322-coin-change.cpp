3f9688cf8049be0f7547727f779630ee8d595c5b
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-11-08 14:45:44 +0200

    Time: 8 ms (99.41%), Space: 16.4 MB (72.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    int foo(int i, int amount, vector<int>& coins) {
-        if(amount == 0) return 0;
-        if(amount < 0 || i == coins.size()) return 1e9;
-        if(memo[i][amount] != -1) return memo[i][amount];
-        return  memo[i][amount] = 
-                    min(0 + foo(i+1, amount, coins),
-                        min(1 + foo(i, amount - coins[i], coins),
-                        1 + foo(i+1, amount - coins[i], coins))
-                    );
-    }
     int coinChange(vector<int>& coins, int amount) {
-        memo = vector<vector<int>>(coins.size(), vector<int>(amount+1, -1));
-        int ans = foo(0, amount, coins);
-        return ans == 1e9 ? -1 : ans;
+        vector<int> dp(amount+1, 1e9);
+        dp[0] = 0;
+        for(int currAmount=1; currAmount<=amount; currAmount++)
+            for(int coin : coins)
+                if(currAmount - coin >= 0)
+                    dp[currAmount] = min(dp[currAmount], 1 + dp[currAmount - coin]);
+        return dp[amount] == 1e9 ? -1 : dp[amount];
     }
 };
+    // vector<vector<int>> memo;
+    // int foo(int i, int amount, vector<int>& coins) {
+    //     if(amount == 0) return 0;
+    //     if(amount < 0 || i == coins.size()) return 1e9;
+    //     if(memo[i][amount] != -1) return memo[i][amount];
+    //     return  memo[i][amount] = 
+    //                 min(0 + foo(i+1, amount, coins),
+    //                     min(1 + foo(i, amount - coins[i], coins),
+    //                     1 + foo(i+1, amount - coins[i], coins))
+    //                 );
+    // }
+        // memo = vector<vector<int>>(coins.size(), vector<int>(amount+1, -1));
+        // int ans = foo(0, amount, coins);
+        // return ans == 1e9 ? -1 : ans;
