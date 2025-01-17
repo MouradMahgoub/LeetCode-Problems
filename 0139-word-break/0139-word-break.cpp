6cecbe749822322eca4883b850323231a0061565
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-01-17 15:56:11 +0200

    Time: 14 ms (35.08%), Space: 16.3 MB (32.65%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,16 +1,42 @@
 class Solution {
 public:
+    unordered_map<string, int> mp;
+    string str = "";
+    vector<int> memo;
+
+    bool foo (int start) {
+        if(start == str.size()) return true;
+        if(memo[start] != -1) return memo[start];
+
+        string temp = "";
+        for(int i=start; i<str.size(); i++) {
+            temp += str[i];
+            if(mp[temp] && foo(i+1)) return (memo[start] = 1);
+        }
+        
+        return (memo[start] = 0);
+    }
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size()+1, false);
-        dp[s.size()] = true;
-        for(int i=s.size()-1; ~i; i--)
-            for(string word : wordDict)
-                if(i+word.size()-1 < s.size() && s.substr(i, word.size()) == word && dp[i+word.size()]){
-                    dp[i] = true; break;
-                }
-        return dp[0];
+        for(string word : wordDict) mp[word]++;
+        memo = vector<int>(s.size(), -1);
+        str = s; 
+        return foo(0);
     }
 };
+
+
+
+
+
+        // vector<bool> dp(s.size()+1, false);
+        // dp[s.size()] = true;
+        // for(int i=s.size()-1; ~i; i--)
+        //     for(string word : wordDict)
+        //         if(i+word.size()-1 < s.size() && s.substr(i, word.size()) == word && dp[i+word.size()]){
+        //             dp[i] = true; break;
+        //         }
+        // return dp[0];
         // this->s = s; this->wordDict = wordDict;
         // this->memo = vector<int>(s.size(), -1);
         // return dp(s.size()-1);
