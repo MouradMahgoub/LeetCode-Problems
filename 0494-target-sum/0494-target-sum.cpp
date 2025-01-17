fbb78eec91c7abe820a26f784462df3aee0058ce
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-01-17 18:13:16 +0200

    Time: 16 ms (58.4%), Space: 25.6 MB (30.88%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    map<pair<int, int>, int> memo;
+    vector<vector<int>> memo;
+    int totalSum; 
 
     int foo (int i, int currSum, int target, vector<int>& nums) {
         if(i == nums.size()) return currSum == target ? 1 : 0;
-        if(memo.find({i, currSum}) != memo.end()) return memo[{i, currSum}];
-        return memo[{i, currSum}] = foo(i+1, currSum + nums[i], target, nums) +
+        if(memo[i][totalSum + currSum] != -1) return memo[i][totalSum + currSum];
+        return memo[i][totalSum + currSum] = foo(i+1, currSum + nums[i], target, nums) +
                                   foo(i+1, currSum - nums[i], target, nums);
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
+        totalSum = accumulate(nums.begin(), nums.end(), 0);
+        memo = vector<vector<int>> (nums.size(), vector<int>(2*totalSum + 1, -1));
         return foo(0, 0, target, nums);
     }
 };
