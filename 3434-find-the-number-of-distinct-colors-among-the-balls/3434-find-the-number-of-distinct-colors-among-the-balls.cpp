ce398d19ad1c2f12b636514671f1266a31eaf749
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-02-07 17:08:42 +0200

    Time: 99 ms (95.82%), Space: 155.6 MB (89.39%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        map<int, int> ballColor;
-        map<int, set<int>> colorBalls;
-        vector<int> ans(queries.size(), 0);
-        int indx = 0;
-        for(auto q : queries){
-            int ball = q[0], newColor = q[1];
-            if(ballColor.find(ball) != ballColor.end()){
-                int oldColor = ballColor[ball];
-                colorBalls[oldColor].erase(ball);
-                if(colorBalls[oldColor].size() == 0) colorBalls.erase(oldColor);
+        unordered_map<int, int> colorOfBall;
+        unordered_map<int, int> numOfBallswithColor;
+        int n = queries.size();
+        vector<int> ans(n, 0);
+        for(int i=0; i<n; i++){
+            int ball = queries[i][0], color = queries[i][1];
+            if(colorOfBall.find(ball) != colorOfBall.end()){
+                int oldColor = colorOfBall[ball];
+                numOfBallswithColor[oldColor]--;
+                if(numOfBallswithColor[oldColor] == 0) numOfBallswithColor.erase(oldColor);
             }
-            colorBalls[newColor].insert(ball);
-            ballColor[ball] = newColor;
-            ans[indx++] = colorBalls.size();
+            colorOfBall[ball] = color;
+            numOfBallswithColor[color]++;
+            ans[i] = numOfBallswithColor.size();
         }
         return ans;
     }
