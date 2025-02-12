aeb0f4e63bb64c47b65d289c8822d30288850cb5
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-02-12 19:48:44 +0200

    Time: 0 ms (100%), Space: 9.3 MB (90.63%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    bool check(stack<char> &stk, string &part){
-        stack<char> temp = stk;
-        for(int i=part.size()-1; i>=0; i--) {
-            if(temp.top() != part[i])
-                return false;
-            temp.pop();
-        }
-        return true;
+     string removeOccurrences(string s, string part) {
+        while(s.find(part) != string::npos)
+            s.erase(s.find(part), part.size());
+        return s;
     }
-
-    string removeOccurrences(string s, string part) {
-        stack<char> stk;
-        int sLen = s.size(), pLen = part.size();
-        for(int i=0; i<sLen; i++){
-            stk.push(s[i]);
-            if(stk.size() >= pLen && check(stk, part)){
-                for(int j=0; j<pLen; j++) stk.pop();
-            }
-        }
-        string ans = "";
-        while(!stk.empty()){
-            ans = stk.top() + ans;
-            stk.pop();
-        }
-        return ans;
-    }
-
 
 };
+    // bool check(stack<char> &stk, string &part){
+    //     stack<char> temp = stk;
+    //     for(int i=part.size()-1; i>=0; i--) {
+    //         if(temp.top() != part[i])
+    //             return false;
+    //         temp.pop();
+    //     }
+    //     return true;
+    // }
+
+    // string removeOccurrences(string s, string part) {
+    //     stack<char> stk;
+    //     int sLen = s.size(), pLen = part.size();
+    //     for(int i=0; i<sLen; i++){
+    //         stk.push(s[i]);
+    //         if(stk.size() >= pLen && check(stk, part)){
+    //             for(int j=0; j<pLen; j++) stk.pop();
+    //         }
+    //     }
+    //     string ans = "";
+    //     while(!stk.empty()){
+    //         ans = stk.top() + ans;
+    //         stk.pop();
+    //     }
+    //     return ans;
+    // }
