20b7ab407ba7465916882228013eea3e343dbc86
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-02-12 20:10:19 +0200

    Time: 1420 ms (14.72%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,11 +1,8 @@
 # Write your MySQL query statement below
-select distinct product_id, 10 as price
+select product_id, 10 as price
 from Products
-where product_id not in (
-        select product_id
-        from Products
-        where change_date <= '2019-08-16'
-    )
+group by product_id
+having min(change_date) > '2019-08-16'
 union all
 select product_id, new_price as price
 from Products
