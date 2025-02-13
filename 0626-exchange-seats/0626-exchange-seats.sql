8318645ae133aee34b11906e57dbf57f16414e0a
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-02-14 01:17:35 +0200

    Time: 536 ms (69.63%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,11 +1,13 @@
 # Write your MySQL query statement below
 select 
-    case
-        when mod(id, 2) = 1 and id = (select max(id) from Seat)
-            then id
-        when mod(id, 2) = 0
-            then id-1
-        else id+1
-    end as id, student
+    if(id = (select max(id) from Seat) and mod(id, 2) = 1, id, 
+        if(mod(id, 2) = 0, id-1, id+1)) as id, student
 from Seat
 order by id
+    -- case
+    --     when mod(id, 2) = 1 and id = (select max(id) from Seat)
+    --         then id
+    --     when mod(id, 2) = 0
+    --         then id-1
+    --     else id+1
+    -- end as id, student
