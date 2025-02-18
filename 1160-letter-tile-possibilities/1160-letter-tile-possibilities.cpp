5ec5c7e4e53be8ad3428653a48d87274f4f0d136
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-02-18 23:00:36 +0200

    Time: 1 ms (83.39%), Space: 8 MB (74.45%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int numTilePossibilities(string tiles) {
-        // Track frequency of each uppercase letter (A-Z)
-        int charCount[26] = {0};
-        for (char c : tiles) {
-            charCount[c - 'A']++;
-        }
-
-        // Find all possible sequences using character frequencies
-        return findSequences(charCount);
-    }
-
-private:
-    int findSequences(int charCount[26]) {
-        int totalCount = 0;
-
-        // Try using each available character
-        for (int pos = 0; pos < 26; pos++) {
-            if (charCount[pos] == 0) {
-                continue;
+    unordered_map<char, int> mp;
+    int dfs () {
+        int  cnt = 0;
+        for(auto [c, n] : mp){
+            if(n > 0) {
+                mp[c]--;
+                cnt += 1 + dfs();
+                mp[c]++;
             }
-
-            // Add current character and recurse
-            totalCount++;
-            charCount[pos]--;
-            totalCount += findSequences(charCount);
-            charCount[pos]++;
         }
-
-        return totalCount;
+        return cnt;
+    }
+    int numTilePossibilities(string s) {
+        for(char c : s) mp[c]++;
+        return dfs();
     }
 };
