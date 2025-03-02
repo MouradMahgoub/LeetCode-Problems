ebc2ab6145901374598c519530cf2e77e93af8a5
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-02 13:12:43 +0200

    Time: 0 ms (100%), Space: 14.9 MB (44.73%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -4,13 +4,9 @@ public:
         int i = 0, j = 0;
         vector<vector<int>> ans;
         while(i < v1.size() && j < v2.size()){
-            if(v1[i][0] < v2[j][0]){
-                ans.push_back({v1[i++]});
-            }else if(v1[i][0] > v2[j][0]){
-                ans.push_back({v2[j++]});
-            }else{
-                ans.push_back({v1[i][0], v1[i++][1] + v2[j++][1]});
-            }
+            if(v1[i][0] < v2[j][0]) ans.push_back({v1[i++]});
+            else if(v1[i][0] > v2[j][0]) ans.push_back({v2[j++]});
+            else ans.push_back({v1[i][0], v1[i++][1] + v2[j++][1]});
         }
         while(i < v1.size()) ans.push_back(v1[i++]);
         while(j < v2.size()) ans.push_back(v2[j++]);
