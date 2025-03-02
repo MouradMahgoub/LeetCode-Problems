e3d25221cbbd4ab7060285e14f883ad4798dcf43
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-02 13:11:39 +0200

    Time: 0 ms (100%), Space: 14.8 MB (44.73%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& v1, vector<vector<int>>& v2) {
-        map<int, int> mp;
-        for(auto v : v1) mp[v[0]] = v[1];
-        for(auto v : v2) mp[v[0]] += v[1];
+        int i = 0, j = 0;
         vector<vector<int>> ans;
-        for(auto it : mp) ans.push_back({it.first, it.second});
+        while(i < v1.size() && j < v2.size()){
+            if(v1[i][0] < v2[j][0]){
+                ans.push_back({v1[i++]});
+            }else if(v1[i][0] > v2[j][0]){
+                ans.push_back({v2[j++]});
+            }else{
+                ans.push_back({v1[i][0], v1[i++][1] + v2[j++][1]});
+            }
+        }
+        while(i < v1.size()) ans.push_back(v1[i++]);
+        while(j < v2.size()) ans.push_back(v2[j++]);
         return ans;
     }
 };
+        // map<int, int> mp;
+        // for(auto v : v1) mp[v[0]] = v[1];
+        // for(auto v : v2) mp[v[0]] += v[1];
+        // vector<vector<int>> ans;
+        // for(auto it : mp) ans.push_back({it.first, it.second});
+        // return ans;
