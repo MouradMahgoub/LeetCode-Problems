a50375d31e4cd7a7d2d992bded25b626740506d4
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-05 17:16:35 +0200

    Time: 0 ms (100%), Space: 7.7 MB (84.76%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int dp[100] = {0};
+    int dp[38] = {0};
     
     Solution() {
         dp[1] = 1;
         dp[2] = 1;
+        for(int i=3; i<38; i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]; 
     }
     
     int tribonacci(int n) {
-        if(!n) return 0;
-        if(dp[n]) return dp[n];
-        dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
         return dp[n];
     }
 };
