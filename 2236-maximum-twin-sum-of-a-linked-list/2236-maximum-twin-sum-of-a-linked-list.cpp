c36a9ca5380665fe1e1b32ae4cad6b178522b9d3
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-05 17:10:08 +0200

    Time: 3 ms (68.74%), Space: 124.2 MB (82.23%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,25 +10,48 @@
  */
 class Solution {
 public:
+    ListNode* reverseList (ListNode* head) {
+        ListNode * curr = head, * prev = NULL;
+        while(curr){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     int pairSum(ListNode* head) {
-        ListNode* temp = head;
-        int n = 0;
-        while(temp){
-            n++;
-            temp = temp->next;
+        ListNode* slow = head, * fast = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        vector<int> v(n/2, 0);
-        temp = head;
-        for(int i=0; i<n/2; i++){
-            v[i] = temp->val;
-            temp = temp->next;
-        }
-        for(int i=n/2 - 1; i>=0; i--){
-            v[i] += temp->val;
-            temp = temp->next;
-        }
+        ListNode* tail = reverseList(slow);
         int ans = -1e9;
-        for(int num : v) ans = max(ans, num);
+        while(tail){
+            ans = max(ans, head->val + tail->val);
+            head = head->next;
+            tail = tail->next;
+        }
         return ans;
     }
 };
+        // ListNode* temp = head;
+        // int n = 0;
+        // while(temp){
+        //     n++;
+        //     temp = temp->next;
+        // }
+        // vector<int> v(n/2, 0);
+        // temp = head;
+        // for(int i=0; i<n/2; i++){
+        //     v[i] = temp->val;
+        //     temp = temp->next;
+        // }
+        // for(int i=n/2 - 1; i>=0; i--){
+        //     v[i] += temp->val;
+        //     temp = temp->next;
+        // }
+        // int ans = -1e9;
+        // for(int num : v) ans = max(ans, num);
+        // return ans;
