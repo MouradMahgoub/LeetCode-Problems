1be0cb598e6db1901292ab71aed21fde579ac18a
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-06 16:51:46 +0200

    Time: 0 ms (100%), Space: 9.9 MB (30.97%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int n, m;
+    string s1, s2, s3;
+    vector<vector<int>> memo;
+    bool foo(int i, int j){
+        if(memo[i][j] != -1) return memo[i][j];
+        if(i + j == s3.size()) return memo[i][j] = true;
+        if(i == n) {
+            while(j < m) {
+                if(s3[i+j] != s2[j]) return memo[i][j] = false;
+                j++;
+            }
+            return memo[i][j] = true;
+        }
+        if(j == m){
+            while(i < n) {
+                if(s3[i+j] != s1[i]) return memo[i][j] = false;
+                i++;
+            }
+            return memo[i][j] = true;
+        }
+
+        if(s3[i+j] == s1[i] && foo(i+1, j)) return memo[i][j] = true; 
+        if(s3[i+j] == s2[j] && foo(i, j+1)) return memo[i][j] = true; 
+        return memo[i][j] = false;
+    }
+    bool isInterleave(string ss1, string ss2, string ss3) {
+        n = ss1.size(); m = ss2.size();
+        memo.resize(n+1, vector<int>(m+1, -1));
+        if(n + m != ss3.size()) return false;
+        s1 = ss1; s2 = ss2; s3 = ss3;
+        return foo(0, 0);
+    }
+};
