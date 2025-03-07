e953496dd949d956035d3d0c6f98be894d7266c7
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-07 13:49:22 +0200

    Time: 10 ms (35.47%), Space: 13.3 MB (25.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    int foo (int i, int j, string& s1, string& s2) {
-        if(memo[i][j] != -1) return memo[i][j];
-        if(i == s1.size()) return memo[i][j] = s2.size() - j;
-        if(j == s2.size()) return memo[i][j] = s1.size() - i;
-        if(s1[i] == s2[j]) 
-            return memo[i][j] = foo(i+1, j+1, s1, s2);
-        return memo[i][j] = 1 + min({foo(i+1, j+1, s1, s2), foo(i+1, j, s1, s2), foo(i, j+1, s1, s2)});
-
-    }
     int minDistance(string s1, string s2) {
-        memo.resize(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return foo(0, 0, s1, s2);
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=1; i<n+1; i++) dp[i][0] = i;
+        for(int j=1; j<m+1; j++) dp[0][j] = j;
+        for(int i=1; i<n+1; i++)
+            for(int j=1; j<m+1; j++)
+                if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+        return dp[n][m];
     }
 
+    // vector<vector<int>> memo;
+    // int foo (int i, int j, string& s1, string& s2) {
+    //     if(memo[i][j] != -1) return memo[i][j];
+    //     if(i == s1.size()) return memo[i][j] = s2.size() - j;
+    //     if(j == s2.size()) return memo[i][j] = s1.size() - i;
+    //     if(s1[i] == s2[j]) 
+    //         return memo[i][j] = foo(i+1, j+1, s1, s2);
+    //     return memo[i][j] = 1 + min({foo(i+1, j+1, s1, s2), foo(i+1, j, s1, s2), foo(i, j+1, s1, s2)});
 
+    // }
+    //     memo.resize(s1.size()+1, vector<int>(s2.size()+1, -1));
+    //     return foo(0, 0, s1, s2);
 
         // int n=s1.size(), m=s2.size();
         // vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1, 0));
