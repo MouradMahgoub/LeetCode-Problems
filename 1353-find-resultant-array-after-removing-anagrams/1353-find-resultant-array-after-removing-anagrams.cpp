5de51b64ba15f2c9ca68298308e91d34277d6adf
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-07 17:31:57 +0200

    Time: 3 ms (56.24%), Space: 15.9 MB (91.21%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> ans;
+        ans.push_back(words[0]);
+        for(int i=1; i<words.size(); i++){
+            string curr = words[i], prev = words[i-1];
+            sort(curr.begin(),curr.end());
+            sort(prev.begin(),prev.end());
+            if(curr != prev) ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
