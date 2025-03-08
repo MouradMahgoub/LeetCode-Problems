cef948b4b20e642e372e9a119e15e9e358710ad6
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-08 13:57:45 +0200

    Time: 3 ms (73.52%), Space: 14.8 MB (48.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& v, int x) {
-        unordered_map<int, int> mp;
-        for(int i=0; i<v.size(); i++) {
-            mp[v[i]] = i;
-        }
-        for(int i=0; i<v.size(); i++) {
-            int indx = mp[x - v[i]];
-            if(indx != 0 && indx != i){
-                cout << i << " " << indx;
-                return {i, indx};
-            }
+    vector<int> twoSum(vector<int>& nums, int x) {
+        vector<pair<int, int>> v(nums.size());
+        for(int i=0; i<nums.size(); i++) v[i] = {nums[i], i};
+        sort(v.begin(), v.end());
+        int i=0, j=v.size()-1;
+        while(i < j){
+            int currSum = v[i].first + v[j].first;
+            if(currSum < x) i++;
+            else if(currSum > x) j--;
+            else return {v[i].second, v[j].second};
         }
         return {};
     }
 };
+        // unordered_map<int, int> mp;
+        // for(int i=0; i<v.size(); i++) {
+        //     mp[v[i]] = i;
+        // }
+        // for(int i=0; i<v.size(); i++) {
+        //     int indx = mp[x - v[i]];
+        //     if(indx != 0 && indx != i){
+        //         cout << i << " " << indx;
+        //         return {i, indx};
+        //     }
+        // }
+        // return {};
