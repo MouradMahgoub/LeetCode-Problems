32f4ee69a446468402873ce229b970054960fdf2
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-03-17 00:38:32 +0200

    Time: 55 ms (46.88%), Space: 59.2 MB (74.87%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    bool good (vector<int>& ranks, int cars, long ans) {
-        long sum = 0;
-        for(int r : ranks) sum += (long)sqrt(1.0*ans/r);
-        return sum >= cars;
-    } 
+    bool check(vector<int>& ranks, int cars, long long time){
+        long numOfcars = 0;
+        for(int r : ranks) numOfcars += (long)sqrt(1.0*time/r);
+        return numOfcars >= cars;
+    }
     long long repairCars(vector<int>& ranks, int cars) {
-        long l=0, r=1e18;
-        while(l < r){
-            long mid = l + (r-l)/2;
-            if(good(ranks, cars, mid)) r = mid;
-            else l = mid + 1;
+        long long l=0, r=1e18;
+        long long ans=-1;
+        while(l <= r){
+            long long mid = l + (r-l)/2;
+            if(check(ranks, cars, mid)) {
+                ans = mid;
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
         }
-        return l;
+        return ans;
     }
 };
+    // bool good (vector<int>& ranks, int cars, long ans) {
+    //     long sum = 0;
+    //     for(int r : ranks) sum += (long)sqrt(1.0*ans/r);
+    //     return sum >= cars;
+    // } 
+    // long long repairCars(vector<int>& ranks, int cars) {
+    //     long l=0, r=1e18;
+    //     while(l < r){
+    //         long mid = l + (r-l)/2;
+    //         if(good(ranks, cars, mid)) r = mid;
+    //         else l = mid + 1;
+    //     }
+    //     return l;
+    // }
