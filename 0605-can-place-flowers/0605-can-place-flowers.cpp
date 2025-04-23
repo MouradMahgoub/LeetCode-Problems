ba935696a3097e78caec131d590a774776f06255
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-04-23 23:07:26 +0200

    Time: 0 ms (100%), Space: 24.2 MB (40.07%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int prev1 = 0, aft1 = 0;
-        int curr = 0, add = 0, ans = 0;
-        for(int i=0; i<flowerbed.size(); i++){
-            if(flowerbed[i] == 0) curr++;
-            else {
-                ans += (curr-prev1)/2;
-                prev1 = 1;
-                curr = 0;
+        int ans = 0, m = flowerbed.size();
+        for(int i=0; i<m; i++){
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == m-1 || flowerbed[i+1] == 0)){
+                flowerbed[i] = 1;
+                ans++;
             }
             if(ans >= n) return true;
         }
-        ans += (curr-prev1+1)/2;
-        cout << ans ;
         return ans >= n;
     }
 };
+        // int prev1 = 0, aft1 = 0;
+        // int curr = 0, add = 0, ans = 0;
+        // for(int i=0; i<flowerbed.size(); i++){
+        //     if(flowerbed[i] == 0) curr++;
+        //     else {
+        //         ans += (curr-prev1)/2;
+        //         prev1 = 1;
+        //         curr = 0;
+        //     }
+        //     if(ans >= n) return true;
+        // }
+        // ans += (curr-prev1+1)/2;
+        // cout << ans ;
+        // return ans >= n;
