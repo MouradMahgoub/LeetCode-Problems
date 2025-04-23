2fa3fd1199f714ab50cb3b4456ad89cf5e754914
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-04-24 00:01:48 +0200

    Time: 5 ms (19.29%), Space: 10.4 MB (53.91%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        int i=0, j=s.size()-1;
-        while(i < j){
-            i = s.find_first_of("aeiouAEIOU", i);
-            j = s.find_last_of("aeiouAEIOU", j);
-            if(i < j) swap(s[i++], s[j--]);
+        int l=0, r=s.size()-1;
+        string vowels = "aeiouAEIOU";
+        while(l < r){
+            while(l < r && vowels.find(s[l]) == -1) l++;
+            while(l < r && vowels.find(s[r]) == -1) r--;
+            swap(s[l], s[r]);
+            l++;
+            r--;
         }
         return s;
     }
 };
+        // int i=0, j=s.size()-1;
+        // while(i < j){
+        //     i = s.find_first_of("aeiouAEIOU", i);
+        //     j = s.find_last_of("aeiouAEIOU", j);
+        //     if(i < j) swap(s[i++], s[j--]);
+        // }
+        // return s;
