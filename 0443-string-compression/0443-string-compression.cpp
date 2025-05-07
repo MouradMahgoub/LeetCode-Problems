caf15914a1cc35289291606c43ac3e4002be862a
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-05-07 19:43:19 +0300

    Time: 1 ms (21.97%), Space: 13.7 MB (74.35%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,16 +3,11 @@ public:
     int compress(vector<char>& chars) {
         int n = chars.size(), i = 0,currIndx = 0;
         while(i < n){
-            int cnt = 0;
-            int j = i;
-            while(j<n && chars[i] == chars[j]) j++, cnt++;
-            if(cnt == 1) chars[currIndx++] = chars[i];
-            else {
-                chars[currIndx++] = chars[i];
-                string s = to_string(cnt);
-                for(char c : s) chars[currIndx++] = c;
-            }
-            i = j;
+            int cnt = 1;
+            while(i + cnt < n &&  chars[i] == chars[i + cnt]) cnt++;
+            chars[currIndx++] = chars[i];
+            if(cnt > 1) for(char c : to_string(cnt)) chars[currIndx++] = c;
+            i += cnt;
         }
         return currIndx;
     }
