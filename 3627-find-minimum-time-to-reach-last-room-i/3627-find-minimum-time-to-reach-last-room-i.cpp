4f9f9d7ac6e550e6b9cff1af674eb41fe7d16db6
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-05-07 19:00:27 +0300

    Time: 63 ms (9.63%), Space: 37 MB (9.26%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size(), m = moveTime[0].size();
+        vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
+        vector<vector<bool>> visited(n, vector<bool> (m, false));
+        priority_queue<vector<int>> pq;
+        pq.push({-0, 0, 0});
+        // visited[0][0] = true;
+        // minTime[0][0] = 0;
+        int dirs[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        while(!pq.empty()){
+            auto v = pq.top(); pq.pop();
+            int currTime = -v[0], i = v[1], j = v[2];
+            visited[i][j] = true;
+            if(currTime >= minTime[i][j]) continue;
+            minTime[i][j] = currTime;
+            if(i == n-1 && j == m-1) return currTime;
+            for(int idx=0; idx<4; idx++){
+                int ii = i + dirs[idx][0], jj = j + dirs[idx][1];
+                if(ii < 0 || ii > n-1 || jj < 0 || jj > m-1 || visited[ii][jj]) continue;
+                int newTime = max(currTime, moveTime[ii][jj]) + 1;
+                pq.push({-newTime, ii, jj});
+                // visited[i][j] = true;
+            }
+        }
+        return -1;
+    }
+};
