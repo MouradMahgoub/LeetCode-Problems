e6ebcbb79e911e63008cf0532a19c456f18a05b8
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-05-07 19:04:39 +0300

    Time: 38 ms (22.47%), Space: 33.8 MB (16.42%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -6,22 +6,21 @@ public:
         vector<vector<bool>> visited(n, vector<bool> (m, false));
         priority_queue<vector<int>> pq;
         pq.push({-0, 0, 0});
-        // visited[0][0] = true;
-        // minTime[0][0] = 0;
         int dirs[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
         while(!pq.empty()){
             auto v = pq.top(); pq.pop();
             int currTime = -v[0], i = v[1], j = v[2];
+            if(visited[i][j]) continue;
             visited[i][j] = true;
-            if(currTime >= minTime[i][j]) continue;
-            minTime[i][j] = currTime;
             if(i == n-1 && j == m-1) return currTime;
             for(int idx=0; idx<4; idx++){
                 int ii = i + dirs[idx][0], jj = j + dirs[idx][1];
-                if(ii < 0 || ii > n-1 || jj < 0 || jj > m-1 || visited[ii][jj]) continue;
+                if(ii < 0 || ii > n-1 || jj < 0 || jj > m-1) continue;
                 int newTime = max(currTime, moveTime[ii][jj]) + 1;
-                pq.push({-newTime, ii, jj});
-                // visited[i][j] = true;
+                if(newTime < minTime[ii][jj]){
+                    minTime[ii][jj] = newTime;
+                    pq.push({-newTime, ii, jj});
+                }
             }
         }
         return -1;
