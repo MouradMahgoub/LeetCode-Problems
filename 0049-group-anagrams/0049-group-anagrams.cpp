8cf1b5261d9a1408b8baff5943fae5aefd51c0ba
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-05-14 22:51:19 +0300

    Time: 19 ms (49.28%), Space: 26.1 MB (48.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for(int i=0; i<strs.size(); i++){
-            string s = strs[i];
-            sort(s.begin(), s.end());
-            //found
-            if(mp.find(s) != mp.end()){
-                mp[s].push_back(strs[i]);
-            //NOt found
-            }else{
-                mp[s] = {strs[i]};
-            }
-        }
         vector<vector<string>> ans;
-        for(auto [s, v] : mp) ans.push_back(v);
+        unordered_map<string, vector<string>>mp;
+        for(string s : strs){
+            string temp = s;
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(s);
+        }
+        for(auto [s, v] : mp)
+            ans.push_back(v);
         return ans;
     }
 };
+
+
+        // unordered_map<string, vector<string>> mp;
+        // for(int i=0; i<strs.size(); i++){
+        //     string s = strs[i];
+        //     sort(s.begin(), s.end());
+        //     //found
+        //     if(mp.find(s) != mp.end()){
+        //         mp[s].push_back(strs[i]);
+        //     //NOt found
+        //     }else{
+        //         mp[s] = {strs[i]};
+        //     }
+        // }
+        // vector<vector<string>> ans;
+        // for(auto [s, v] : mp) ans.push_back(v);
+        // return ans;
