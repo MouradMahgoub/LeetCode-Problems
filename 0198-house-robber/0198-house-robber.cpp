23a5eced1e055275e12e2a3ab675151463bce482
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-05-15 14:58:33 +0300

    Time: 0 ms (100%), Space: 10.8 MB (22.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
+    vector<int> memo;
+    int foo(vector<int>& nums, int i){
+        if(i > nums.size()-1) return 0 ;
+        if(memo[i] != -1) return memo[i];
+        return memo[i] = max(nums[i] + foo(nums, i+2), foo(nums, i+1));
+    }
     int rob(vector<int>& nums) {
-        int prev1 = 0, prev2=0;
-        for(int num : nums){
-            int temp = prev1;
-            prev1 = max(prev2 + num, prev1);
-            prev2 = temp;
-        }
-        return prev1;
+        memo = vector<int>(nums.size(), -1);
+        return foo(nums, 0);
     }
 };
+        // int prev1 = 0, prev2=0;
+        // for(int num : nums){
+        //     int temp = prev1;
+        //     prev1 = max(prev2 + num, prev1);
+        //     prev2 = temp;
+        // }
+        // return prev1;
+        
         // vector<int> dp(nums.size()+1, 0);
         // dp[0] = nums[0];
         // dp[1] = max(nums[0], nums[1]);
