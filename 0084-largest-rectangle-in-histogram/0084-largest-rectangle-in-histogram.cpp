82485451d27d4521378e03f846366ba52d3e4f9d
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-06-19 02:12:32 +0300

    Time: 11 ms (95.09%), Space: 81.2 MB (80.95%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<pair<int, int>> st;
-        int ans = 0, n = heights.size();
+        int n = heights.size();
+        int ans = 0;
+        stack<int> st;
         for(int i=0; i<n; i++){
-            int indx = i;
-            while(!st.empty() && heights[i] < st.top().first) {
-                indx = st.top().second;
-                int hei = st.top().first, width = i - indx;
-                ans = max(ans, hei*width);
+            int newIndx = i;
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                newIndx = st.top();
+                ans = max(ans, (i - st.top())*heights[st.top()]);
                 st.pop();
             }
-            st.push({heights[i], indx});
+            heights[newIndx] = heights[i];
+            st.push(newIndx);
         }
         while(!st.empty()){
-            ans = max(ans, (st.top().first)*(n-st.top().second));
+            ans = max(ans, (n - st.top())*heights[st.top()]);
             st.pop();
         }
         return ans;
     }
 };
+
+
+        // stack<pair<int, int>> st;
+        // int ans = 0, n = heights.size();
+        // for(int i=0; i<n; i++){
+        //     int indx = i;
+        //     while(!st.empty() && heights[i] < st.top().first) {
+        //         indx = st.top().second;
+        //         int hei = st.top().first, width = i - indx;
+        //         ans = max(ans, hei*width);
+        //         st.pop();
+        //     }
+        //     st.push({heights[i], indx});
+        // }
+        // while(!st.empty()){
+        //     ans = max(ans, (st.top().first)*(n-st.top().second));
+        //     st.pop();
+        // }
+        // return ans;
