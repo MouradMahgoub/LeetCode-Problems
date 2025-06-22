7f760db31ef8992722d910d6a84398031135c215
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-06-22 08:30:04 +0300

    Time: 578 ms (90.67%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,8 +1,23 @@
-# Write your MySQL query statement below
-select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage 
+select round(100*avg(order_date = customer_pref_delivery_date), 2) immediate_percentage
 from Delivery
-where (customer_id, order_date) in (
-        select  customer_id, min(order_date)
+where (customer_id, order_date) in 
+    (
+        select customer_id, min(order_date)
         from Delivery
         group by customer_id
     )
+
+
+
+
+
+
+
+-- # Write your MySQL query statement below
+-- select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage 
+-- from Delivery
+-- where (customer_id, order_date) in (
+--         select  customer_id, min(order_date)
+--         from Delivery
+--         group by customer_id
+--     )
