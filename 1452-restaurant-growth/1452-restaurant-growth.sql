a691afd6e3706cd9cbea798daf12ed096d16a74c
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-06-23 07:46:42 +0300

    Time: 720 ms (5.02%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -2,9 +2,7 @@
 select C1.visited_on, sum(C2.amount) amount, round(sum(C2.amount)/7, 2) average_amount
 from (
     select distinct visited_on from Customer
-    where date_sub(visited_on, interval 6 day) in (
-        select visited_on from Customer
-    )
+    where visited_on >= date_add((select min(visited_on) from Customer), interval 6 day)
 ) C1
 join Customer C2 
 on C2.visited_on between date_sub(C1.visited_on, interval 6 day) and C1.visited_on
