3c24a9d3a717600fc3ff8f60bec7387f2cc663ff
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2025-06-23 08:20:51 +0300

    Time: 497 ms (53.18%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,21 @@
-# Write your MySQL query statement below
-select activity_date as day, count(distinct user_id) as active_users
-from Activity where activity_date between '2019-06-28' and '2019-07-27'
+select activity_date day, count(distinct user_id) active_users
+from Activity
+where activity_date between '2019-06-28' and '2019-07-27'
 group by activity_date
+
+
+
+
+
+
+
+
+
+
+
+
+-- # Write your MySQL query statement below
+-- select activity_date as day, count(distinct user_id) as active_users
+-- from Activity 
+-- where activity_date between '2019-06-28' and '2019-07-27'
+-- group by activity_date
